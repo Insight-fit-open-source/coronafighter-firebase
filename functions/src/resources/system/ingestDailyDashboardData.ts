4409ed0f6228be038dac1080c98177e6d4148978
Shaun Egan <shauneganza@gmail.com>
2020-03-31 12:55:55 +0200

    refactor(logs): Better log message

--- functions/src/resources/system/ingestDailyDashboardData.ts
+++ functions/src/resources/system/ingestDailyDashboardData.ts
@@ -5,7 +5,7 @@ import { DashboardService } from '../../DashboardService'
 const dashboardService = container.resolve('dashboardService') as DashboardService
 
 export default functions.pubsub.schedule('every 1 hour').onRun(() => {
-  console.log('Updating dashboard data')
+  console.log('Updating daily data')
 
   return dashboardService.ingestDaily()
 })
